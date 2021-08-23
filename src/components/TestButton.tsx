37bfd2f26bd6ec470550f63212ab33e35f47e067
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-23 16:37:05 +0300

    Change to arrow functions, experiment with alert and window.open

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,14 @@
 function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
+  return (
+    <button
+      onClick={() => { 
+        console.log("Click for the window.open reference")
+        window.open("https://www.w3schools.com/jsref/met_win_open.asp")
+      }}
+    >
+      Make me do something when I'm clicked...
+    </button>
+  );
 }
 
 export default TestButton;
